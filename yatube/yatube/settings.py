21052af6614393de40e9df0a3c5c0287468e260d
Екатерина Василевич <katerinair8@mail.ru>
2022-06-26 22:01:56 +0300

    Создание первого  приложения

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',    
+]
