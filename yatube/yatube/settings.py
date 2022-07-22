102c1a64f961f36b606f5fa6b0c1a6395cb1b7c0
Екатерина Василевич <katerinair8@mail.ru>
2022-07-22 13:15:32 +0300

    HTML страницы в Django. Render

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -128,3 +128,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',    
 ]
+from django.shortcuts import render
+
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],        
+        'APP_DIRS': True, 
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        },
+    }
+] 
